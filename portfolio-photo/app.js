e76e91e8f16c0981e7b797cc1367109f0182bc21
Andrey Fanin <77449797+andrey-fanin@users.noreply.github.com>
2021-03-19 18:29:49 +0500

    Добавил слайдер на Index.html
    Простецкий слайдер на чистом js. Начало положено!

---
+++ portfolio-photo/app.js
@@ -0,0 +1,35 @@
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("hero__list");
+    var dots = document.getElementsByClassName("pagination__item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("pagination__item--active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " pagination__item--active";
+}
